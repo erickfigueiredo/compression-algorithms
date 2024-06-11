7b384852c3a52ef73c1c80329476abded6a25b22
Erick Figueiredo <ericklima816@gmail.com>
2024-06-10 22:54:02 -0300

    feat: Adiciona representação em string de quadrante

--- compression/quadrant.py
+++ compression/quadrant.py
@@ -2,6 +2,13 @@ import numpy as np
 from copy import deepcopy
 
 
+class QuadrantRepresentationError(Exception):
+    """
+    Exception to raise when the Quadrant color is not set.
+    """
+    pass
+
+
 class Quadrant:
     """
     A class to represent a quadrant (Node) in a quadtree.
@@ -10,18 +17,27 @@ class Quadrant:
     def __init__(self, x: int, y: int, width: int, height: int, depth: int = 0) -> None:
         """
         Constructor for the Quadrant class.
-        :param x: Origin X-coordinate of the quadrant in the quadtree.
-        :param y: Origin Y-coordinate of the quadrant in the quadtree.
+        :param x: origin X-coordinate of the quadrant in the quadtree.
+        :param y: origin Y-coordinate of the quadrant in the quadtree.
         :param width: Width of the quadrant in the quadtree.
         :param height: Height of the quadrant in the quadtree.
         :param depth: Depth of the quadrant in the quadtree.
         """
-        self.origin = {'x': x, 'y': y}
+        self.__origin = {'x': x, 'y': y}
         self.__width = width
         self.__height = height
 
         self.__depth = depth
         self.__children = []
+        self.__color = None
+
+    @property
+    def origin(self) -> dict:
+        """
+        Getter for the origin of the quadrant.
+        :return: Origin of the quadrant.
+        """
+        return deepcopy(self.__origin)
 
     @property
     def width(self) -> int:
@@ -53,7 +69,7 @@ class Quadrant:
         Getter for the children of the quadrant.
         :return: All children nodes of the quadrant.
         """
-        return deepcopy(self.__children)
+        return self.__children
 
     @children.setter
     def children(self, children: list) -> None:
@@ -77,8 +93,8 @@ class Quadrant:
         :param image: Target image to extract the quadrant from.
         :return: Quadrant of the image.
         """
-        return image[self.origin['y']:self.origin['y'] + self.__height,
-               self.origin['x']:self.origin['x'] + self.__width]
+        return image[self.__origin['y']:self.__origin['y'] + self.__height,
+               self.__origin['x']:self.__origin['x'] + self.__width]
 
     def calc_error(self, image: np.ndarray) -> float:
         """
@@ -87,14 +103,27 @@ class Quadrant:
         :return: Error (std) of the quadrant considering the mean color.
         """
         quadrant = self.get_quadrant_from_image(image)
-        mean_color = self.__calc_mean_color(quadrant)
+        mean_color = self.calc_mean_color(quadrant)
 
         return np.sqrt(np.mean((quadrant - mean_color) ** 2))
 
-    def __calc_mean_color(self, quadrant: np.ndarray) -> np.ndarray:
+    def calc_mean_color(self, quadrant: np.ndarray) -> np.ndarray:
         """
         Method to calculate the mean color of the quadrant.
         :param quadrant: Target quadrant to calculate the mean color from.
         :return: Mean color of the quadrant.
         """
-        return [int(p) for p in np.mean(quadrant, axis=(0, 1))]
+        if not self.__color:
+            self.__color = [int(p) for p in np.mean(quadrant, axis=(0, 1))]
+
+        return self.__color
+
+    def __str__(self) -> str:
+        """
+        String representation of the Quadrant class.
+        :return: String representation of the Quadrant class.
+        """
+        if not self.__color:
+            raise QuadrantRepresentationError("Quadrant color is not set. Please calculate the mean color first!")
+
+        return f"{self.__origin['x']};{self.__origin['y']};{self.__origin['x'] + self.__width};{self.__origin['y'] + self.__height};{','.join(str(l) for l in self.__color)}"
