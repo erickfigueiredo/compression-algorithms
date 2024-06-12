42056e5846703a63753f17de181bfc7b84022985
Erick Figueiredo <ericklima816@gmail.com>
2024-06-11 21:57:14 -0300

    feat: Adiciona documentação aos métodos da quadtree

--- compression/quadtree.py
+++ compression/quadtree.py
@@ -7,7 +7,16 @@ from compression.quadrant import Quadrant
 
 
 class QuadTree:
+    """
+    QuadTree class implementation for Image Compression.
+    """
     def __init__(self, image: np.ndarray, threshold: int = None, min_quad_size: int = 1) -> None:
+        """
+        Constructor for the QuadTree class.
+        :param image: Image to be compressed.
+        :param threshold: Threshold value for quadrant split.
+        :param min_quad_size: Minimum size of the quadrant.
+        """
         self.__image = deepcopy(image)
 
         self.__threshold = threshold
@@ -18,14 +27,25 @@ class QuadTree:
 
     @property
     def max_depth(self) -> int:
+        """
+        Getter for the maximum depth of the quadtree.
+        """
         return self.__get_max_depth(self.root)
 
-    def get_leaves(self) -> list[Quadrant]:
-        return self.__get_leaves(self.root)
+    def get_chidren(self) -> list[Quadrant]:
+        """
+        Getter for the leaves of the quadtree.
+        """
+        return self.__get_chidren(self.root)
 
     def get_compressed_image(self, show_quadrants: bool = False, highlight_color: tuple = (0, 255, 0)) -> np.ndarray:
+        """
+        Method to get the compressed image from the quadtree.
+        :param show_quadrants: Flag to show the quadrants highlighted in the compressed image.
+        :param highlight_color: Color to highlight the quadrants.
+        """
         compressed_image = np.zeros_like(self.__image)
-        children = self.get_leaves()
+        children = self.get_chidren()
 
         for child in children:
             quadrant = child.get_quadrant_from_image(self.__image)
@@ -41,17 +61,27 @@ class QuadTree:
         return compressed_image
 
     def build_compressed_file(self, save_path:str=None, filename:str=None) -> None:
+        """
+        Method to build a compressed file from the quadtree. creates a file with ".lima" extension which contains the
+        instructions for the compressed file construction.
+        :param save_path: Path to save the compressed file.
+        :param filename: Name of the compressed file.
+        """
         if not (save_path and filename):
             raise ValueError('Please provide a save path and filename!')
 
-        quadrants = self.get_leaves()
+        quadrants = self.get_chidren()
         compressed_data = '&'.join([str(quad) for quad in quadrants])
 
-        with open(os.path.join(save_path, f'{filename}.lima'), 'wb') as file:
-            file.write(str.encode(compressed_data))
+        with open(os.path.join(save_path, f'{filename}.lima'), 'w') as file:
+            file.write(f'{self.__image.shape[0]};{self.__image.shape[1]}&{compressed_data}')
 
 
     def __get_max_depth(self, quadrant: Quadrant) -> int:
+        """
+        Method to get the maximum depth of the quadtree recursively.
+        :param quadrant: Quadrant to get the depth from.
+        """
         if not quadrant.children:
             return quadrant.depth
 
@@ -61,17 +91,25 @@ class QuadTree:
 
         return max_depth
 
-    def __get_leaves(self, quadrant: Quadrant) -> list[Quadrant]:
+    def __get_chidren(self, quadrant: Quadrant) -> list[Quadrant]:
+        """
+        Method to get the leaves of the quadtree recursively.
+        :param quadrant: Quadrant to get the leaves from.
+        """
         if not quadrant.children:
             return [quadrant]
 
         leaves = []
         for child in quadrant.children:
-            leaves.extend(self.__get_leaves(child))
+            leaves.extend(self.__get_chidren(child))
 
         return leaves
 
     def __build_tree(self, quadrant: Quadrant) -> None:
+        """
+        Method to build the quadtree recursively.
+        :param quadrant: Quadrant to be split.
+        """
 
         if self.__threshold and quadrant.calc_error(self.__image) <= self.__threshold:
             return
